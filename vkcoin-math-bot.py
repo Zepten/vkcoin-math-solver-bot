69bcd3a8177b5871721ba5eb7262a89d2a3c1e40
Zepten <39930495+Zepten@users.noreply.github.com>
2021-08-16 08:27:49 +0300

    big changes: 12 level recognition + refactoring

--- vkcoin-math-bot.py
+++ vkcoin-math-bot.py
@@ -1,106 +1,101 @@
-import win32api, win32con
 from config import *
 import numpy as np
 import pytesseract
 import threading
-import pyautogui
+import pyautogui as pg
 import keyboard
-import numexpr
 import time
+import math
 import cv2
-import os
 
 
-def replace_all(text: str, d: dict):
-    for i, j in d.items():
-        text = text.replace(i, j)
-    return text
+def mouse_click(pos: tuple) -> None:
+    pg.moveTo(*pos, 0.2, pg.easeOutQuad)
+    pg.click()
 
 
-def get_level_pos(level: int):
-    return (
-        LEVEL1_POS[0] + (level - 1) % 4 * 156,
-        LEVEL1_POS[1] + (level - 1) // 4 * 60
-    )
-
-
-def mouse_click(pos: tuple):
-    win32api.SetCursorPos(pos)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-
-
-def is_captcha_on_screen():
+def is_captcha_on_screen() -> bool:
     # TODO: Pixel Matching
-    return pyautogui.locateOnScreen('captcha.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.8) is not None
+    return pg.locateOnScreen('captcha.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.8) is not None and \
+    pg.locateOnScreen('captcha_vknext.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.8) is not None
 
 
-def input_captcha():
+def input_captcha() -> None:
     print('Waitng for captcha...')
-    captcha = pyautogui.prompt(title='Captcha')
+    captcha = pg.prompt(title='Captcha')
     if captcha:
         mouse_click(CAPTCHA_INPUT_POS)
-        pyautogui.hotkey('ctrl', 'a')
-        pyautogui.press('backspace')
-        pyautogui.write(captcha)
-        pyautogui.press('enter')
+        pg.hotkey('ctrl', 'a')
+        pg.press('backspace')
+        pg.write(captcha)
+        pg.press('enter')
 
 
-def click(pos: tuple):
+def click(pos: tuple[int, int]) -> None:
     while is_captcha_on_screen():
         input_captcha()
         time.sleep(0.5)
     mouse_click(pos)
 
 
-def exit_handler():
+def exit_handler() -> None:
     pause.set()
     while True:
-        if keyboard.wait('escape') is None:
+        if keyboard.wait('q') is None:
             pause.clear()
             print('Stopped.')
 
 
-def bot_handler():
+def number_from_screenshot(number_region: tuple[int, int , int, int]) -> int:
+    image = cv2.cvtColor(np.array(pg.screenshot(region=number_region)), cv2.COLOR_RGB2BGR)
+    return int(pytesseract.image_to_string(image, lang='eng', config=TESSERACT_CONFIG).strip())
+
+
+def bot_handler() -> None:
     click(FOCUS_POS)
     click(PLAY_POS)
     time.sleep(0.5)
+    count = 0
     while pause.wait():
-        print('>----- Start -----<')
-        click(get_level_pos(level))
-        time.sleep(1)
-        image = pyautogui.screenshot(region=EXPRESSION_REGION)
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-        print(f'Screenshot taken.')
-        expression = replace_all(str(pytesseract.image_to_string(image, lang='eng', config=TESSERACT_CONFIG)).strip(), REPLACEMENTS)
-        print(f'Expression: {expression}')
+        count += 1
+        print(f'>----- Attempt #{count} -----<')
+        click(LEVEL12_POS)
+        time.sleep(1.5)
         try:
-            result = str(int(numexpr.evaluate(expression)))
-            print(f'Result: {result}')
-            click(INPUT_POS)
-            pyautogui.hotkey('ctrl', 'a')
-            pyautogui.press('backspace')
-            pyautogui.write(result)
-            pyautogui.press('enter')
-            time.sleep(0.5)
-        except (SyntaxError, KeyError):
-            click(BACK_POS)
-            time.sleep(0.5)
-            click(PLAY_POS)
-            time.sleep(0.5)
+            e1 = number_from_screenshot(N_REG[0] + N_REG_SIZE) # Числитель числа #1
+            d1 = number_from_screenshot(N_REG[1] + N_REG_SIZE) # Знаменатель числа #1
+            e2 = number_from_screenshot(N_REG[2] + N_REG_SIZE) # Числитель числа #2
+            d2 = number_from_screenshot(N_REG[3] + N_REG_SIZE) # Знаменатель числа #2
+        except ValueError:
+            print('Numbers are not recognized.')
+            continue
+        if pg.locateOnScreen('mult.png', region=OPER_REG, grayscale=True, confidence=0.8) is not None:
+            oper = '*'
+        else:
+            oper = '/'
+        print(f'Expression: ({e1}/{d1}) {oper} ({e2}/{d2})')
+        if oper == '/':
+            e2, d2 = d2, e2
+        gcd = math.gcd(e1 * e2, d1 * d2)
+        re = int(e1 * e2 / gcd)
+        rd = int(d1 * d2 / gcd)
+        if rd == 1:
+            result = str(re)
+        else:
+            result = f'{re}/{rd}'
+        print(f'Result: {result}')
+        click(INPUT_POS)
+        pg.hotkey('ctrl', 'a')
+        pg.press('backspace')
+        pg.write(result)
+        pg.press('enter')
+        time.sleep(1)
 
 
-def main():
-    if not os.path.exists('temp'):
-        os.mkdir('temp')
+def main() -> None:
     pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
-    global level
-    try:
-        level = max(1, int(input('Enter level: ')))
-    except:
-        level = DEFAULT_LEVEL
-    print(f'Your level is {level}')
+    print('Go to menu in the game and press enter to start...')
+    input('(To interrupt the process press Q)')
     global pause
     pause = threading.Event()
     bot_thread = threading.Thread(target=bot_handler)
