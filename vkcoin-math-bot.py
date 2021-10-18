01443a553c461515078aa0a9e5d1a891b0004c16
Zepten <39930495+Zepten@users.noreply.github.com>
2021-10-18 17:21:12 +0300

    updated for vknext, better output, waiting mult.

--- vkcoin-math-bot.py
+++ vkcoin-math-bot.py
@@ -1,23 +1,20 @@
-from dotenv import load_dotenv
+import vk_captchasolver as vc
 import pyautogui as pg
-from os import environ
 from config import *
 import pytesseract
 import threading
 import keyboard
-import telebot
 import time
 import math
 
 
-load_dotenv()
-bot = telebot.TeleBot(environ.get('TOKEN'))
-CHAT_ID = environ.get('CHAT_ID')
+def sleep(t: int):
+    time.sleep(t * WAIT_MULT)
 
 
 def is_captcha_on_screen() -> bool:
-    return pg.locateOnScreen('captcha.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.8) is not None and \
-    pg.locateOnScreen('captcha_vknext.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.8) is not None
+    return pg.locateOnScreen('captcha.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.5) is not None and \
+    pg.locateOnScreen('captcha_vknext.png', region=CAPTCHA_REGION, grayscale=True, confidence=0.5) is not None
 
 
 def click(pos: tuple[int, int]) -> None:
@@ -26,16 +23,12 @@ def click(pos: tuple[int, int]) -> None:
 
 
 def input_captcha() -> None:
-    print('Waitng for captcha...')
     pg.screenshot('captcha_temp.png', region=CAPTCHA_IMAGE_REGION)
-    bot.send_photo(chat_id=CHAT_ID, photo=open('captcha_temp.png', 'rb'))
-    captcha = pg.prompt(title='Captcha')
+    captcha = vc.solve(image='captcha_temp.png')
+    print('Captcha solved:', captcha)
     click(CAPTCHA_INPUT_POS)
-    pg.hotkey('ctrl', 'a')
-    pg.press('backspace')
     pg.write(captcha)
     pg.press('enter')
-    bot.send_message(chat_id=CHAT_ID, text='Каптча введена')
 
 
 def exit_handler() -> None:
@@ -46,26 +39,26 @@ def exit_handler() -> None:
             print('Stopped.')
 
 
-def number_from_screenshot(number_region: tuple[int, int , int, int]) -> int:
+def number_from_screenshot(number_region: tuple[int, int, int, int]) -> int:
     screenshot = pg.screenshot(region=number_region)
     return int(pytesseract.image_to_string(screenshot, config=TESSERACT_CONFIG).strip())
 
 
 def bot_handler() -> None:
-    bot.send_message(chat_id=CHAT_ID, text='Бот начал работу')
     click(FOCUS_POS)
     click(PLAY_POS)
-    time.sleep(0.5)
+    sleep(0.5)
     count = 0
+    xp_gain = 0
     while pause.wait():
         while is_captcha_on_screen():
+            print('Captcha is found.')
             input_captcha()
-            time.sleep(1)
-
+            sleep(1)
         count += 1
         print(f'>----- Attempt #{count} -----<')
         click(LEVEL12_POS)
-        time.sleep(1)
+        sleep(1)
         try:
             e1 = number_from_screenshot(N_REG[0] + N_REG_SIZE) # Числитель числа #1
             print(e1, end='')
@@ -92,44 +85,28 @@ def bot_handler() -> None:
             result = str(re)
         else:
             result = f'{re}/{rd}'
-        print(f'Result: {result}')
+        xp_gain += 12
+        print(f'Result: {result}; xp gain: {xp_gain}')
         click(INPUT_POS)
         pg.hotkey('ctrl', 'a')
         pg.press('backspace')
         pg.write(result)
         pg.press('enter')
-        time.sleep(1)
-
-
-@bot.message_handler(func=lambda m: True)
-def telegram_input(message):
-    if is_captcha_on_screen():
-        print(f'Telegram input: "{message.text}"')
-        pg.hotkey('ctrl', 'a')
-        pg.press('backspace')
-        pg.write(message.text)
-        pg.press('enter')
-
-
-def telegram_handler():
-    bot.polling(none_stop=True)
+        sleep(1)
 
 
 def main() -> None:
     pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
     print('Go to menu in the game and press enter to start...')
-    input('(To interrupt the process press Q)')
+    input('(To interrupt the process press Tab)')
     global pause
     pause = threading.Event()
     bot_thread = threading.Thread(target=bot_handler)
     exit_thread = threading.Thread(target=exit_handler)
-    telegram_thread = threading.Thread(target=telegram_handler)
     bot_thread.start()
     exit_thread.start()
-    telegram_thread.start()
     bot_thread.join()
     exit_thread.join()
-    telegram_thread.join()
 
 
 if __name__ == '__main__':
